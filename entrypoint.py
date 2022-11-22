b982164831c640dfdff2cdeb93fba570e6921982
Antonino Sirchia <asirchia@gmail.com>
2022-11-22 14:52:10 +0100

    [FEAT] Calculate age from birthdate
    If date_of_birth column is present in the original dataframe, the age is calculated and added as new column
    
    Ref. https://tasks.office.com/epu.ntua.gr/Home/Task/dcvN3CxgD0u8eVa_wqcO5ZcAD2RU

--- entrypoint.py
+++ entrypoint.py
@@ -1,6 +1,26 @@
 from mescobrad_edge.plugins.edge_plugin_anonymize.models.plugin import EmptyPlugin, PluginActionResponse, PluginExchangeMetadata
+from datetime import date
 
 class GenericPlugin(EmptyPlugin):
+    
+    def age(today, birthdate):
+        # A bool that represents if today's day/month precedes the birth day/month
+        one_or_zero = ((today.month, today.day) < (birthdate.month, birthdate.day))
+    
+        # Calculate the difference in years from the date object's components
+        year_difference = today.year - birthdate.year
+    
+        # The difference in years is not enough. 
+        # To get it right, subtract 1 or 0 based on if today precedes the 
+        # birthdate's month/day.
+    
+        # To do this, subtract the 'one_or_zero' boolean 
+        # from 'year_difference'. (This converts
+        # True to 1 and False to 0 under the hood.)
+        age = year_difference - one_or_zero
+    
+        return age
+    
 
     def action(self, input_meta: PluginExchangeMetadata = None) -> PluginActionResponse:
         import os
@@ -14,19 +34,25 @@ class GenericPlugin(EmptyPlugin):
         if not os.path.isdir(folder):
             os.mkdir(folder)
 
-        columns_to_remove = ["name", "surname"]
+        columns_to_remove = ["name", "surname", "date_of_birth"]
         # load input data
         for file_name in files_to_anonymize:
-            data = pd.read_csv("./" + file_name)
+            data = pd.read_csv("./" + file_name, parse_dates=['date_of_birth'], dayfirst=True)
 
             # Generate list of unique ids
             personal_data = data.loc[:, columns_to_remove]
             list_id = []
+            
             for i in range(data.shape[0]):
                 personal_id = "".join(personal_data.iloc[i])
                 id = hashlib.sha256(bytes(personal_id, "utf-8")).hexdigest()
                 list_id.append(id)
+                
             data.insert(0, "PID", list_id)
+            if(if 'date_of_birth' in personal_data.columns:
+               today = date.today()
+               data['age'] = age(today, data['date_of_birth'])
+               
             data.to_csv("./" + file_name, index=False)
 
             # remove columns with personal information from the CSV files
